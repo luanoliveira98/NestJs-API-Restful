07e4cd6fe25e8c9bfa7018f1f40c37882873d624
Luan Oliveira <luanoliveira17@hotmail.com>
2023-04-04 16:49:48 -0300

    feat: add user entity

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserRepository {
-  private users = [];
+  private users: UserEntity[] = [];
 
-  async store(user) {
+  async store(user: UserEntity) {
     this.users.push(user);
   }
 
